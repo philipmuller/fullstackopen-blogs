59b7cf31df23280860d5c09381d95ca052709af3
Philip Müller <philipmuller97@gmail.com>
2024-05-10 02:35:15 +0300

    Complete 4.18: Blog List Expansion, step 6

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
 console.log('connecting to', mongoUrl)
@@ -16,5 +17,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
